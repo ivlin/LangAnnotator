a3b0c2cb916e50ddac152f9d48674ba346455c83
minivan <ivlin112@gmail.com>
2020-01-28 22:20:29 -0500

    Updates

---
+++ precache-manifest.146d350451b01af5b5930de47df61cb2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cfcbfe44cbc2239c71bb2e59f580df3",
+    "url": "/LangAnnotator/index.html"
+  },
+  {
+    "revision": "ed42e79b86c02c460477",
+    "url": "/LangAnnotator/static/css/main.ca3acb98.chunk.css"
+  },
+  {
+    "revision": "8606282805f55435c7db",
+    "url": "/LangAnnotator/static/js/2.eef5bc31.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/LangAnnotator/static/js/2.eef5bc31.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ed42e79b86c02c460477",
+    "url": "/LangAnnotator/static/js/main.287b18c5.chunk.js"
+  },
+  {
+    "revision": "1c53354ee5864003a3b8",
+    "url": "/LangAnnotator/static/js/runtime-main.db24354b.js"
+  }
+]);
